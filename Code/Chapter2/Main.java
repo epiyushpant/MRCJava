8f345ab851a63a32d4454600ceef103a5b0dbcfc
Piyush Pant <piyushpant@gmail.com>
2025-12-14 20:10:48 +0545

    add chapt 2 start and chap1 codes

---
+++ Code/Chapter2/Main.java
@@ -0,0 +1,16 @@
+ public class Main {
+    public static void main(String[] args) {
+	 System.out.println(args[0]);
+	 System.out.println(args[1]);
+	 
+	 
+	// int num1 = Integer.parseInt(args[0]);
+	// int num2 = Integer.parseInt(args[1]);
+	// int  sum = num1 +num2 ; 
+	// System.out.println(sum);
+	 }
+}	
+
+//to run it using command line  
+// javac Main.java 
+//java Main Hello 123
