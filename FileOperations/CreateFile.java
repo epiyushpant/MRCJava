cc086c63cdfaee106db212a0bc72be574742199e
pradipshrestha0819 <pradipshrestha109@gmail.com>
2026-03-21 20:52:03 +0545

    Add CreateFile.java

---
+++ FileOperations/CreateFile.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        String fileName = "sample.txt";
+        
+        try {
+            File file = new File(fileName);
+            
+            // Check if file already exists
+            if (file.createNewFile()) {
+                System.out.println("File created successfully: " + file.getName());
+                System.out.println("File path: " + file.getAbsolutePath());
+                System.out.println("File size: " + file.length() + " bytes");
+            } else {
+                System.out.println("File already exists!");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
